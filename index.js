6ae96619aa7f548eb43ee594f2c5ee84b829c33e
phieutn <120315973+phieutn@users.noreply.github.com>
2022-12-14 19:29:20 +0700

    Revert "test"
    This reverts commit a3e320524ae13c7bdf6aa2f83f86926d97cb560d.

--- index.js
+++ index.js
@@ -22,9 +22,9 @@ function nhapMang()
     let j = 0;
     let fla = true;
     const arr1=[]
-    for(i=0; i < numMang.length; i++)
+    for(i=0; i < numMang.length - 1; i++)
     {
-        for(j=i+1; j < numMang.length + 1; j++)
+        for(j=i+1; j < numMang.length; j++)
         {
         if(numMang[i]!=numMang[j]){
             fla = true;
